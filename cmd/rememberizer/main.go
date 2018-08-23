4fad22f4cb3e78a91fd89ba35c91a8981f1af8a4
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2018-08-23 21:29:03 +0200

    better cli output

--- cmd/rememberizer/main.go
+++ cmd/rememberizer/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/go-passwd/rememberizer"
 )
 
 func main() {
 	for _, arg := range os.Args[1:] {
-		fmt.Println(arg, ": ", rememberizer.ToSentences(arg, nil))
+		fmt.Println(arg, ":", strings.Join(rememberizer.ToSentences(arg, nil), " "))
 	}
 }
